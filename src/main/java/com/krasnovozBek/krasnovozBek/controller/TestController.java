6f0251659183fb1b839aa53470a3f7e699e5358e
Vira Vznk <veravznk@gmail.com>
2026-04-13 23:00:47 +0300

    first commit

--- src/main/java/com/krasnovozBek/krasnovozBek/controller/TestController.java
+++ src/main/java/com/krasnovozBek/krasnovozBek/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
 
+
     @GetMapping("/test")
     public String test() {
         return "hi";
