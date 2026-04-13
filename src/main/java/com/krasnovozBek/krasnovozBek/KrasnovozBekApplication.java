bd7e5c3b7cd67c3d6f3a3f613b1b2c8f9ba28a1f
Vira Vznk <veravznk@gmail.com>
2026-04-13 23:18:50 +0300

    2 comm

--- src/main/java/com/krasnovozBek/krasnovozBek/KrasnovozBekApplication.java
+++ src/main/java/com/krasnovozBek/krasnovozBek/KrasnovozBekApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KrasnovozBekApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(KrasnovozBekApplication.class, args);
 	}
