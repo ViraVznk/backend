2312a32d15a24d56f3eab12fa1979070e8074b80
Anna Krasno <annakrasn2212@gmail.com>
2026-04-18 00:13:39 +0300

    controller upd

--- src/main/java/com/krasnovozBek/krasnovozBek/controller/TestController.java
+++ src/main/java/com/krasnovozBek/krasnovozBek/controller/TestController.java
@@ -1,21 +1,46 @@
 package com.krasnovozBek.krasnovozBek.controller;
+import com.krasnovozBek.krasnovozBek.dao.CategoryDao;
+import com.krasnovozBek.krasnovozBek.domain.Category;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/categories")
 @CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
     private final JdbcTemplate jdbc;
-    public TestController(JdbcTemplate jdbc) {
+
+    private final CategoryDao categoryDao;
+
+    public TestController(JdbcTemplate jdbc, CategoryDao categoryDao) {
         this.jdbc = jdbc;
+        this.categoryDao = categoryDao;
     }
-    @GetMapping
-    public List<Map<String, Object>> getAll() {
+
+    @GetMapping("/api/categories")
+    public List<Map<String, Object>> getAllCategories() {
+        log.info("in categories");
         return jdbc.queryForList("SELECT * FROM Category");
+
     }
+
+    @GetMapping("/api/products")
+    public List<Map<String, Object>> getAllProducts() {
+        log.info("in products");
+        return jdbc.queryForList("SELECT * FROM Product");
+    }
+
+    @PostMapping("/api/categories")
+    public ResponseEntity<Void> create(@RequestBody Category category) {
+        log.info("trying to add new category");
+        categoryDao.create(category);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
